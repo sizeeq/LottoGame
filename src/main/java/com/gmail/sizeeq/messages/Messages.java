8f4068639e081852c94ca814dd2ee6b938c7b075
BuildTools <unconfigured@null.spigotmc.org>
2023-12-28 17:49:42 +0100

    Whole game implementation without test.

--- src/main/java/com/gmail/sizeeq/messages/Messages.java
+++ src/main/java/com/gmail/sizeeq/messages/Messages.java
@@ -8,7 +8,7 @@ public class Messages {
     public static final String ENTER_NUMBERS = String.format("Please enter %d numbers!", Config.NUMBERS_FROM_USER);
     public static final String ENTER_NUMBER = String.format("Please enter number from %d-%d!", Config.LOWER_BOUND, Config.UPPER_BOUND);
     public static final String NUMBER_NOT_IN_RANGE = String.format("The number you've entered is not in range %d-%d", Config.LOWER_BOUND, Config.UPPER_BOUND);
-    public static final String RESULT_MESSAGE = "You've managed to hit %d numbers! Winning numbers %s, your numbers %s";
+    //public static final String RESULT_MESSAGE = "You've managed to hit %d numbers! Winning numbers %s, your numbers %s";
     public static final String WINNING_RESULT_MESSAGE = "You've won! %d numbers hit! Winning numbers %s, your numbers %s";
     public static final String LOSING_RESULT_MESSAGE = "You've lost! %d numbers hit! Winning numbers %s, your numbers %s";
 }
